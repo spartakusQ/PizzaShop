0a38dbf999c27b48056173c1d2e58745c9a63cd1
spartakusQ <spartakus1688@gmail.com>
2017-03-19 14:16:12 +0300

    Create Pizza migration

---
+++ db/migrate/20170318182126_add_products.rb
@@ -0,0 +1,13 @@
+class AddProducts < ActiveRecord::Migration
+  def change
+
+
+    Product.create :title => 'Hawaiian', :description => 'This is Haeaiian pizza', :price => 350, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/hawaiian.jpg'
+
+    Product.create :title => 'Pepperoni', :description => 'Nice Pepperoni pizza', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
+
+    Product.create :title => 'Vegetarian', :description => 'Amazing Vegeterian pizza', :price => 400, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/veg.jpg'
+
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318175250) do
+ActiveRecord::Schema.define(version: 20170318182126) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
