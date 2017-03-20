3964df12596bb60c38e5727c8c440ae525dc6ebf
spartakusQ <spartakus1688@gmail.com>
2017-03-20 16:36:42 +0300

    Update orders input

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-function something()
-{
+function something(){
 		var x = window.localStorage.getItem('bbb');
 
 		x = x * 1 + 1;
@@ -11,20 +10,29 @@ function something()
 	}
 
 
-function add_to_cart(id)
-{
+function add_to_cart(id){
 	var key = 'product_' + id
 
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
-	alert('Items in your cart: ' + cart_get_number_of_items());
+	update_orders_input();
 }
 
 
-function cart_get_number_of_items()
-{
+
+function update_orders_input() {
+
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+
+}
+
+
+
+
+function cart_get_number_of_items(){
   var cnt = 0
 
 	for(var i = 0; i < window.localStorage.length; i++)
@@ -42,8 +50,8 @@ function cart_get_number_of_items()
 
 
 
-function orders()
-{
+function cart_get_orders(){
+
   var orders= '';
 
 	for(var i = 0; i < window.localStorage.length; i++)
