379444d748f7d0eb8206426f7dc7ea5933d9302a
spartakusQ <spartakus1688@gmail.com>
2017-03-20 16:16:12 +0300

    Add rost form & cart_get_orders

--- public/app.js
+++ public/app.js
@@ -39,3 +39,22 @@ function cart_get_number_of_items()
 	}
 	return cnt;
 }
+
+
+
+function orders()
+{
+  var orders= '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
+}
