ac686026a24044b83c491ee794a34cfae9ae3ee1
spartakusQ <spartakus1688@gmail.com>
2017-03-22 22:09:50 +0300

    Add to bd orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318182126) do
+ActiveRecord::Schema.define(version: 20170322190317) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
