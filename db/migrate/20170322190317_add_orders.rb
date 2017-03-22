c8406d783b601580c372dbd856b6e6935301e638
spartakusQ <spartakus1688@gmail.com>
2017-03-22 22:18:54 +0300

    Delite  git comments to migration file

--- db/migrate/20170322190317_add_orders.rb
+++ db/migrate/20170322190317_add_orders.rb
@@ -1,6 +1,6 @@
 class AddOrders < ActiveRecord::Migration
   def change
-<<<<<<< HEAD
+
 
 
     create_table :orders do |t|
@@ -16,7 +16,6 @@ class AddOrders < ActiveRecord::Migration
 
 
 
-=======
->>>>>>> master
+
   end
 end
